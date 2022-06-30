fd1bbbf64f64cd07b7875bcb4cf8237e10f1a6d3
Pravin Singh Rana <andaz23@yahoo.co.in>
2022-06-30 20:29:12 +0530

    Bubble Sort

--- array.cpp
+++ array.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <climits>
 using namespace std;
-void selectionSort(int arr[], int n)
+
+// selection sort
+/* void selectionSort(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
@@ -15,10 +17,30 @@ void selectionSort(int arr[], int n)
             }
         }
     }
+} */
+
+// bubble sort
+void bubbleSort(int arr[], int n)
+{
+    int counter = 1;
+    while (counter < n)
+    {
+        for (int i = 0; i < n - counter; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        counter++;
+    }
 }
 
 int main()
 {
+
     /*  int n;
      cin >> n;
      int arr[n];
@@ -43,10 +65,9 @@ int main()
      }
      cout << maxNum << " " << minNum << endl; */
 
-    // selection sort
-    int arr[6] = {1, 2, 4, 7, 12, 14};
+        int arr[6] = {1, 2, 4, 7, 12, 14};
     int arraySize = sizeof(arr) / sizeof(int);
-    selectionSort(arr, arraySize);
+    bubbleSort(arr, arraySize);
     for (int i = 0; i < arraySize; i++)
     {
         cout << arr[i] << endl;
